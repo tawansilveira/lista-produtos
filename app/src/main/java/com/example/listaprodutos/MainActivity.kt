11223aa99c4ad5b360ad195e7b42ddfae858fc50
tawansilveira <tawansilveira@rede.ulbra.br>
2024-06-02 19:43:48 -0300

    navegação home

--- app/src/main/java/com/example/listaprodutos/MainActivity.kt
+++ app/src/main/java/com/example/listaprodutos/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navController = findNavController(R.id.fragmentContainerView) as NavController
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        navView.setupWithNavController(navController)
     }
 }
