95bcbe6cd9042f106c0fb75c40d415aaeabcfd63
tawansilveira <tawansilveira@rede.ulbra.br>
2024-06-03 22:41:50 -0300

    finalizando search bar

--- app/src/main/java/com/example/listaprodutos/HomeFragment.kt
+++ app/src/main/java/com/example/listaprodutos/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -17,21 +18,70 @@ class HomeFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val adapter = ProdutoAdapter(listOf(
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
-            Produto("Passat", "R$ 18.000,00", "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"),
+        val adapter = ProdutoAdapter(::goToDetail)
 
-        ), ::goToDetail)
+        val lista = listOf(
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Passat",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+            Produto(
+                "Santana",
+                "R$ 18.000,00",
+                "https://s2-autoesporte.glbimg.com/Ei2bOmBNdcC3TSJpyVyY99KPmLg=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/t/h/XJQgAVQ5CeGVpTATlF5w/2019-10-03-passatfrente.jpg"
+            ),
+        )
+
+        adapter.setProdutos(lista)
 
         val recyclerView: RecyclerView = view.findViewById(R.id.rv_lista_produtos)
 
         recyclerView.adapter = adapter
+
+        view.findViewById<SearchView>(R.id.search_bar)
+            .setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    adapter.filter.filter(query)
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter.filter.filter(newText)
+                    return false
+                }
+
+            })
 
         return view
     }
--- app/src/main/java/com/example/listaprodutos/ProdutoAdapter.kt
+++ app/src/main/java/com/example/listaprodutos/ProdutoAdapter.kt
@@ -3,19 +3,28 @@ package com.example.listaprodutos
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class ProdutoAdapter(private val produtos: List<Produto>,
-                     private val goToDetail: (product: Produto) -> Unit
-):
-    RecyclerView.Adapter<ProdutoAdapter.ProdutoViewHolder>() {
+class ProdutoAdapter(
+    private val goToDetail: (product: Produto) -> Unit
+) : RecyclerView.Adapter<ProdutoAdapter.ProdutoViewHolder>(), Filterable {
 
+    private var produtos: List<Produto> = emptyList()
+    private var produtosFiltrados: List<Produto> = emptyList()
 
-    inner class ProdutoViewHolder(private val itemView: View): RecyclerView.ViewHolder(itemView){
-        fun bind(produto: Produto){
+    fun setProdutos(list: List<Produto>){
+        produtos = list
+        produtosFiltrados = produtos
+        notifyDataSetChanged()
+    }
+
+    inner class ProdutoViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(produto: Produto) {
             val imgProduto: ImageView = itemView.findViewById(R.id.imageView)
             val txtNomeProduto: TextView = itemView.findViewById(R.id.txt_produto)
             val txtPrecoProduto: TextView = itemView.findViewById(R.id.txt_produto_preco)
@@ -25,7 +34,7 @@ class ProdutoAdapter(private val produtos: List<Produto>,
 
             Glide.with(itemView.context).load(produto.url).into(imgProduto)
 
-            itemView.setOnClickListener{
+            itemView.setOnClickListener {
                 goToDetail(produto)
             }
         }
@@ -33,14 +42,42 @@ class ProdutoAdapter(private val produtos: List<Produto>,
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdutoViewHolder {
-        val view =  LayoutInflater.from(parent.context).inflate(R.layout.lista_produto_item_layout, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.lista_produto_item_layout, parent, false)
         return ProdutoViewHolder(view)
     }
 
-    override fun getItemCount() = produtos.size
+    override fun getItemCount() = produtosFiltrados.size
 
     override fun onBindViewHolder(holder: ProdutoViewHolder, position: Int) {
-        holder.bind(produtos[position])
+        holder.bind(produtosFiltrados[position])
 
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                produtosFiltrados = if (constraint.isNullOrEmpty()){
+                    produtos
+                } else {
+                   val teste = produtos.filter {it.nome.contains(constraint)}
+                    teste
+                }
+
+                return FilterResults().apply { values = produtosFiltrados as ArrayList<Produto> }
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+
+                produtosFiltrados = if (results?.values == null){
+                    ArrayList()
+                }else{
+                    results.values as ArrayList<Produto>
+                }
+
+                notifyDataSetChanged()
+            }
+
+        }
     }
 }
